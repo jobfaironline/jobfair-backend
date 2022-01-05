27d47f1e96b6704460d20e9f1a30ece4d5f992e9
Son Pham <phamcaoson1999@gmail.com>
2022-01-05 10:33:35 +0700

    spring code baseline

---
+++ src/test/java/com/example/jobfair_be/JobFairBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobfair_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobFairBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
