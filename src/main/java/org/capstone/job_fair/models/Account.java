b320c975f7f7daaf6fb1bc5f5220428532da9e08
Tien T. Truong <tientt1938@gmail.com>
2022-01-07 10:49:53 +0700

    CAP-19 mapping database to account model

--- src/main/java/org/capstone/job_fair/models/Account.java
+++ src/main/java/org/capstone/job_fair/models/Account.java
@@ -2,6 +2,7 @@ package org.capstone.job_fair.models;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,31 +12,53 @@ import javax.persistence.*;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "Account")
+@Table(name = "account")
 public class Account {
     @Id
     @Column(name = "email")
     private String email;
     @Column(name = "password")
     private String password;
-    @Column(name = "fullname")
-    private String fullname;
-    @Column(name = "address")
-    private String address;
+
+    @Column(name = "firstname")
+    private String firstName;
+    @Column(name = "lastname")
+    private String lastName;
+    @Column(name = "middlename")
+    private String middleName;
+    @Column(name = "title")
+    private String title;
     @Column(name = "phone")
     private String phone;
-    @Column(name = "gender")
-    private String gender;
-    @Column(name = "status")
-    private int status;
+    @Column(name = "address")
+    private String address;
     @Column(name = "dob")
-    private float dob;
+    private Long dob;
+    @Column(name = "status")
+    private Integer status;
+    @Column(name = "job_title")
+    private String jobTitle;
+    @Column(name = "current_job_level_id")
+    private Integer currentJobLevelId;
+    @Column(name = "year_of_exp")
+    private Float yearOfExp;
+    @Column(name = "profile_image_url")
+    private String profileImageUrl;
+    @Column(name = "country_id")
+    private Integer countryId;
+    @Column(name = "residence_id")
+    private Integer residenceId;
+
 
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "roleID")
+    @JoinColumn(name = "role_id")
     @JsonBackReference
     private Role role;
+
+    public String getFullname (){
+        return firstName + middleName + lastName;
+    }
 
 
 
