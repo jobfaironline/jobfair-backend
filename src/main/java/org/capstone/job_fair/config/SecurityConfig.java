83f1a547f959a43e7cb55a0da720d8de1e4c9210
Tien T. Truong <tientt1938@gmail.com>
2022-01-07 20:37:13 +0700

    CAP-25 fix some swagger config

--- src/main/java/org/capstone/job_fair/config/SecurityConfig.java
+++ src/main/java/org/capstone/job_fair/config/SecurityConfig.java
@@ -63,7 +63,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 "/swagger-resources/**",
                 "/configuration/security",
                 "/swagger-ui.html",
-                "/webjars/**");
+                "/webjars/**",
+                "/csrf"
+                );
     }
 
     @Override
