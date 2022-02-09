bb4c30f4d1dd0c2739ad5892b0a3ff3ead7f460a
BuiMinhKhoi <44707470+BuiMinhKhoi@users.noreply.github.com>
2022-02-09 16:33:24 +0700

    [CAP-90] Add reason for cancel job fair registration

--- src/main/java/org/capstone/job_fair/controllers/payload/requests/CancelCompanyJobFairRegistrationRequest.java
+++ src/main/java/org/capstone/job_fair/controllers/payload/requests/CancelCompanyJobFairRegistrationRequest.java
@@ -2,8 +2,12 @@ package org.capstone.job_fair.controllers.payload.requests;
 
 
 import lombok.*;
+import org.capstone.job_fair.constants.DataConstraint;
+import org.capstone.job_fair.validators.XSSConstraint;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 @NoArgsConstructor
@@ -15,7 +19,9 @@ public class CancelCompanyJobFairRegistrationRequest {
     @NotNull
     private String companyRegistrationId;
 
-    @NotNull
+    @NotBlank
+    @Size(max = DataConstraint.JobFair.COMPANY_REASON_CANCEL_JOB_FAIR_REGISTRATION_MAX_LENGTH)
+    @XSSConstraint
     private String cancelReason;
 
 }
