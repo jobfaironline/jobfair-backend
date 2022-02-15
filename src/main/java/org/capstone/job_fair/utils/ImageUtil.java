21948bc4f359f994f4b499740e9d07a714d6e7fc
BuiMinhKhoi <44707470+BuiMinhKhoi@users.noreply.github.com>
2022-02-15 15:36:28 +0700

    [CAP-96] Implement upload picture profile

--- src/main/java/org/capstone/job_fair/utils/ImageUtil.java
+++ src/main/java/org/capstone/job_fair/utils/ImageUtil.java
@@ -13,9 +13,10 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class ImageUtil {
-    public static byte[]convertImage(MultipartFile file) throws IOException{
+    public static byte[] convertImage(MultipartFile file) throws IOException {
         return convertImage(file, DataConstraint.Account.IMAGE_TYPE, DataConstraint.Account.WIDTH_FACTOR, DataConstraint.Account.HEIGHT_FACTOR);
     }
+
     public static byte[] convertImage(MultipartFile file, int imageType, double fWidth, double fHeight) throws IOException {
         BufferedImage dbi = null;
 
@@ -23,9 +24,9 @@ public class ImageUtil {
         InputStream is = new ByteArrayInputStream(bytes);
         BufferedImage sbi = ImageIO.read(is);
 
-        if(sbi != null) {
-            int dWidth = (int) (sbi.getWidth()*fWidth);
-            int dHeight = (int) (sbi.getHeight()*fHeight);
+        if (sbi != null) {
+            int dWidth = (int) (sbi.getWidth() * fWidth);
+            int dHeight = (int) (sbi.getHeight() * fHeight);
             dbi = new BufferedImage(dWidth, dHeight, imageType);
             Graphics2D g = dbi.createGraphics();
             AffineTransform at = AffineTransform.getScaleInstance(fWidth, fHeight);
