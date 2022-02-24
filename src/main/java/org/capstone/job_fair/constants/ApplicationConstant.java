c20bdbb969e66d59f0e3e1c4e09348694052d114
BuiMinhKhoi <44707470+BuiMinhKhoi@users.noreply.github.com>
2022-02-24 21:44:35 +0700

    [CAP-119] Rename

--- src/main/java/org/capstone/job_fair/constants/ApplicationConstant.java
+++ src/main/java/org/capstone/job_fair/constants/ApplicationConstant.java
@@ -4,12 +4,12 @@ public final class ApplicationConstant {
     private ApplicationConstant() {
     }
 
-    public static final String DEFAULT_STATUS_VALUE = "DRAFT";
-    public static final String DEFAULT_FROM_TIME_VALUE = "1";
-    public static final String DEFAULT_TO_TIME_VALUE = "1";
-    public static final String DEFAULT_OFFSET_VALUE = "0";
-    public static final String DEFAULT_PAGE_SIZE_VALUE = "10";
-    public static final String DEFAULT_SORT_BY_VALUE = "createDate";
+    public static final String DEFAULT_SEARCH_STATUS_VALUE = "DRAFT";
+    public static final String DEFAULT_SEARCH_FROM_TIME_VALUE = "1";
+    public static final String DEFAULT_SEARCH_TO_TIME_VALUE = "1";
+    public static final String DEFAULT_SEARCH_OFFSET_VALUE = "0";
+    public static final String DEFAULT_SEARCH_PAGE_SIZE_VALUE = "10";
+    public static final String DEFAULT_SEARCH_SORT_BY_VALUE = "createDate";
 
 
 }
