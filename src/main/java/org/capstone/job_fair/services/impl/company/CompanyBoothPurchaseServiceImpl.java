c139076be7ea9a5ae42be625b92fa3252451dadb
Tien T. Truong <53970640+TienTruong2000@users.noreply.github.com>
2022-03-02 22:58:05 +0700

    [HOT-FIX] implement cannot buy more than 1 booth (#98)
    * [HOT-FIX] implement get company booth ID
    
    * [HOT-FIX] implement cannot buy more than 1 booth

--- src/main/java/org/capstone/job_fair/services/impl/company/CompanyBoothPurchaseServiceImpl.java
+++ src/main/java/org/capstone/job_fair/services/impl/company/CompanyBoothPurchaseServiceImpl.java
@@ -4,9 +4,11 @@ import org.capstone.job_fair.constants.MessageConstant;
 import org.capstone.job_fair.models.dtos.company.CompanyBoothDTO;
 import org.capstone.job_fair.models.dtos.company.OrderDTO;
 import org.capstone.job_fair.models.entities.company.CompanyBoothEntity;
+import org.capstone.job_fair.models.entities.company.CompanyRegistrationEntity;
 import org.capstone.job_fair.models.entities.company.OrderEntity;
 import org.capstone.job_fair.models.entities.job_fair.BoothEntity;
 import org.capstone.job_fair.repositories.company.CompanyBoothRepository;
+import org.capstone.job_fair.repositories.company.CompanyRegistrationRepository;
 import org.capstone.job_fair.repositories.company.OrderRepository;
 import org.capstone.job_fair.repositories.job_fair.BoothRepository;
 import org.capstone.job_fair.services.interfaces.company.CompanyBoothPurchaseService;
@@ -18,6 +20,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -39,6 +42,9 @@ public class CompanyBoothPurchaseServiceImpl implements CompanyBoothPurchaseServ
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CompanyRegistrationRepository companyRegistrationRepository;
+
     @Override
     @Transactional
     public CompanyBoothDTO purchaseBooth(String companyRegistrationId, String boothId) {
@@ -46,6 +52,18 @@ public class CompanyBoothPurchaseServiceImpl implements CompanyBoothPurchaseServ
         if (!boothOpt.isPresent()){
             throw new IllegalArgumentException(MessageUtil.getMessage(MessageConstant.Booth.NOT_FOUND));
         }
+
+        Optional<CompanyRegistrationEntity> companyRegistrationOpt = companyRegistrationRepository.findById(companyRegistrationId);
+        if (!companyRegistrationOpt.isPresent()){
+            throw new IllegalArgumentException(MessageUtil.getMessage(MessageConstant.CompanyRegistration.NOT_FOUND));
+        }
+
+        List<CompanyBoothEntity> companyBoothEntities = companyBoothRepository.getCompanyBoothByJobFairIdAndCompanyId(companyRegistrationOpt.get().getJobFairId(), companyRegistrationOpt.get().getCompanyId());
+        if (companyBoothEntities.size() > 0){
+            throw new IllegalArgumentException(MessageUtil.getMessage(MessageConstant.BoothPurchase.ALREADY_HAS_BOOTH));
+        }
+
+
         BoothEntity boothEntity = boothOpt.get();
 
         OrderDTO orderDTO = orderService.createOrder(companyRegistrationId);
