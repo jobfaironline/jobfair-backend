3bac8815e378af17722470924f1e9828c1d9b009
BuiMinhKhoi <44707470+BuiMinhKhoi@users.noreply.github.com>
2022-03-02 15:03:20 +0700

    [CAP-185] job fair API cho admin

--- src/main/java/org/capstone/job_fair/services/impl/job_fair/JobFairServiceImpl.java
+++ src/main/java/org/capstone/job_fair/services/impl/job_fair/JobFairServiceImpl.java
@@ -224,11 +224,11 @@ public class JobFairServiceImpl implements JobFairService {
     public Page<JobFairDTO> getAllForAdmin(JobFairPlanStatus status, int offset, int pageSize, String sortBy, Sort.Direction direction) {
         if (offset < DataConstraint.CompanyRegistration.OFFSET_MIN || pageSize < DataConstraint.CompanyRegistration.PAGE_SIZE_MIN)
             throw new IllegalArgumentException(MessageUtil.getMessage(MessageConstant.JobFair.INVALID_PAGE_NUMBER));
-        if (status.equals(JobFairPlanStatus.DRAFT))
+        if (status != null && status.equals(JobFairPlanStatus.DRAFT))
             throw new IllegalArgumentException(MessageUtil.getMessage(MessageConstant.JobFair.INVALID_JOB_FAIR_SEARCH_STATUS_OF_ADMIN));
         if (status == null) {
-            Page<JobFairDTO> page = jobFairRepository.findAllByStatusNot(JobFairPlanStatus.DRAFT, PageRequest.of(offset, pageSize).withSort(Sort.by(direction, sortBy))).map(entity -> jobFairMapper.toJobFairDTO(entity));
-            return page;
+            Page<JobFairEntity> page = jobFairRepository.findAllByStatusNot(JobFairPlanStatus.DRAFT, PageRequest.of(offset, pageSize).withSort(Sort.by(direction, sortBy)));
+            return page.map(entity -> jobFairMapper.toJobFairDTO(entity));
         }
         return jobFairRepository.findByStatus(status, PageRequest.of(offset, pageSize).withSort(Sort.by(direction, sortBy))).map(entity -> jobFairMapper.toJobFairDTO(entity));
     }
