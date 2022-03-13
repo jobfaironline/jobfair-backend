1c58c8f215215c48f1e48816056e6e47b2299ea5
BuiMinhKhoi <44707470+BuiMinhKhoi@users.noreply.github.com>
2022-03-13 17:05:13 +0700

    [HOT-FIX] Fix attendant, company job fair sort and statuses (#126)

--- src/main/java/org/capstone/job_fair/repositories/job_fair/AttendantJobFairStatusRepository.java
+++ src/main/java/org/capstone/job_fair/repositories/job_fair/AttendantJobFairStatusRepository.java
@@ -1,18 +1,17 @@
 package org.capstone.job_fair.repositories.job_fair;
 
 import org.capstone.job_fair.models.entities.job_fair.AttendantJobFairStatusEntity;
-import org.capstone.job_fair.models.entities.job_fair.CompanyJobFairStatusEntity;
 import org.capstone.job_fair.models.statuses.JobFairAttendantStatus;
-import org.capstone.job_fair.models.statuses.JobFairCompanyStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AttendantJobFairStatusRepository extends JpaRepository<AttendantJobFairStatusEntity, String> {
     Page<AttendantJobFairStatusEntity> getAllByAttendantId(String attendantId, Pageable pageable);
 
-    Page<AttendantJobFairStatusEntity> getByStatusAndAttendantId(JobFairAttendantStatus status, String attendantId, Pageable pageable);
+    Page<AttendantJobFairStatusEntity> getByStatusInAndAttendantId(List<JobFairAttendantStatus> status, String attendantId, Pageable pageable);
 }
