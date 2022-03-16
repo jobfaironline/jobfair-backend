a556611ef328b26ae6aeb5abd30e0f6b35100fda
BuiMinhKhoi <44707470+BuiMinhKhoi@users.noreply.github.com>
2022-03-16 17:03:52 +0700

    [CAP-268] Implement get latest company registration of company by job fair id

--- src/main/java/org/capstone/job_fair/services/impl/company/CompanyRegistrationServiceImpl.java
+++ src/main/java/org/capstone/job_fair/services/impl/company/CompanyRegistrationServiceImpl.java
@@ -390,7 +390,7 @@ public class CompanyRegistrationServiceImpl implements CompanyRegistrationServic
         if (offset < DataConstraint.Paging.OFFSET_MIN || pageSize < DataConstraint.Paging.PAGE_SIZE_MIN)
             throw new IllegalArgumentException(MessageUtil.getMessage(MessageConstant.CompanyRegistration.INVALID_PAGE_NUMBER));
         if (statusList == null || statusList.isEmpty())
-            companyRegistrationEntityPage = companyRegistrationRepository.findAllByStatusIn(Arrays.asList(CompanyRegistrationStatus.APPROVE, CompanyRegistrationStatus.REJECT, CompanyRegistrationStatus.REQUEST_CHANGE), PageRequest.of(offset, pageSize).withSort(Sort.by(direction, sortBy)));
+            companyRegistrationEntityPage = companyRegistrationRepository.findAllByStatusIn(Arrays.asList(CompanyRegistrationStatus.APPROVE, CompanyRegistrationStatus.REJECT, CompanyRegistrationStatus.PENDING, CompanyRegistrationStatus.REQUEST_CHANGE), PageRequest.of(offset, pageSize).withSort(Sort.by(direction, sortBy)));
         else
             companyRegistrationEntityPage = companyRegistrationRepository.findAllByStatusIn(statusList, PageRequest.of(offset, pageSize).withSort(Sort.by(direction, sortBy)));
         return companyRegistrationEntityPage.map(entity -> companyRegistrationMapper.toDTO(entity));
