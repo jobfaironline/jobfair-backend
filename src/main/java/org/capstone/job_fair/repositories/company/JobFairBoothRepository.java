94869de744a4b081045f50c837078979483f86a1
Tien T. Truong <tientt1938@gmail.com>
2022-04-18 00:31:52 +0700

    [CAP-377] - fix remove assignment when pick new layout

--- src/main/java/org/capstone/job_fair/repositories/company/JobFairBoothRepository.java
+++ src/main/java/org/capstone/job_fair/repositories/company/JobFairBoothRepository.java
@@ -2,9 +2,11 @@ package org.capstone.job_fair.repositories.company;
 
 import org.capstone.job_fair.models.entities.company.JobFairBoothEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,7 +21,8 @@ public interface JobFairBoothRepository extends JpaRepository<JobFairBoothEntity
 
     List<JobFairBoothEntity> findByJobFairId(String jobFairId);
 
-    Integer deleteAllByJobFairIdAndBoothLayoutCompanyId(String jobFairId, String companyId);
+    @Modifying
+    Integer deleteAllByJobFairId(String jobFairId);
 
     Integer countByJobFairId(String jobFairId);
 
