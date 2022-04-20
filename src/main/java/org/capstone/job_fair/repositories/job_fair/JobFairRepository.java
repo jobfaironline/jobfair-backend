360e6028a5b84afffd9ba5fdb62a451bd2f53529
Son Pham Cao <62231837+CaoSon1909@users.noreply.github.com>
2022-04-20 08:14:40 +0700

    [CAP-342] - add constraint company cannot create 2 job fairs in the same public time (#167)
    * [CAP-343] - implement get assignment by employee
    
    * [CAP-343] - implement get assignment by id
    
    * [CAP-343] - fix create new layout
    
    * [CAP-343] - fix get job fair information for 3D map
    
    * [CAP-343] - fix employee's assignment get DRAFT job fair
    
    * [CAP-343] - implement add job position for job fair booth
    
    * [CAP-343] - fix logic of decorate start time and endtime
    
    * [CAP-343] - fix booth job position mapper
    
    * [CAP-342] - add constraint company cannot create 2 job fairs in the same public time
    
    * [CAP-342] - add constraint company cannot create 2 job fairs in the same public time
    
    Co-authored-by: Tien T. Truong <tientt1938@gmail.com>

--- src/main/java/org/capstone/job_fair/repositories/job_fair/JobFairRepository.java
+++ src/main/java/org/capstone/job_fair/repositories/job_fair/JobFairRepository.java
@@ -1,19 +1,21 @@
 package org.capstone.job_fair.repositories.job_fair;
 
 import org.capstone.job_fair.models.entities.job_fair.JobFairEntity;
+import org.capstone.job_fair.models.statuses.JobFairPlanStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface JobFairRepository extends JpaRepository<JobFairEntity, String> {
     Optional<JobFairEntity> findByIdAndCompanyId(String jobFairId, String companyId);
 
-
+    List<JobFairEntity> findByCompanyIdAndStatus(String companyId, JobFairPlanStatus status);
     Page<JobFairEntity> findByNameLikeOrNameIsNullAndCompanyId(String name, String companyId, Pageable pageable);
 
 
