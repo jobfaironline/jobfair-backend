14cd7caa66060e522c4a18607071441f22412fe9
Tien T. Truong <53970640+TienTruong2000@users.noreply.github.com>
2022-06-15 21:38:01 +0700

    hot_fix quiz repository (#261)
    Co-authored-by: CaoSon1909 <phamcaoson1999@gmail.com>

--- src/main/java/org/capstone/job_fair/repositories/attendant/quiz/QuizRepository.java
+++ src/main/java/org/capstone/job_fair/repositories/attendant/quiz/QuizRepository.java
@@ -4,6 +4,7 @@ package org.capstone.job_fair.repositories.attendant.quiz;
 import org.capstone.job_fair.models.entities.attendant.test.QuizEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -13,10 +14,10 @@ public interface QuizRepository extends JpaRepository<QuizEntity, String> {
     Optional<QuizEntity> findByApplicationId(String applicationId);
 
     @Query("SELECT t FROM QuizEntity t WHERE t.beginTime < :currentTime AND t.endTime > :currentTime AND t.application.id = :applicationId AND t.application.attendant.accountId = :userId")
-    Optional<QuizEntity> getQuiz(Long currentTime, String applicationId, String userId);
+    Optional<QuizEntity> getQuiz(@Param("currentTime") Long currentTime, @Param("applicationId") String applicationId, @Param("userId") String userId);
 
     @Query("SELECT t FROM QuizEntity t WHERE t.application.attendant.accountId = :userId AND t.id = :quizId")
-    Optional<QuizEntity> findQuizToSave(String quizId, String userId);
+    Optional<QuizEntity> findQuizToSave(@Param("quizId") String quizId, @Param("userId") String userId);
 
     Optional<QuizEntity> findByIdAndApplicationAttendantAccountId(String id, String accountId);
 }
