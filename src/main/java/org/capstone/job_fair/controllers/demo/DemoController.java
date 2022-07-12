1376398f1c6f8bde53579c53cc502a70ba1f016c
BMK <lagaxyvippro@gmail.com>
2022-07-12 16:03:03 +0700

    [DEMO] DEMO API

---
+++ src/main/java/org/capstone/job_fair/controllers/demo/DemoController.java
@@ -0,0 +1,114 @@
+package org.capstone.job_fair.controllers.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.capstone.job_fair.constants.ApiEndPoint;
+import org.capstone.job_fair.controllers.payload.requests.attendant.RegisterAttendantRequest;
+import org.capstone.job_fair.controllers.payload.requests.attendant.cv.UpdateCvRequest;
+import org.capstone.job_fair.models.dtos.account.AccountDTO;
+import org.capstone.job_fair.models.dtos.attendant.AttendantDTO;
+import org.capstone.job_fair.models.dtos.attendant.cv.CvDTO;
+import org.capstone.job_fair.services.interfaces.attendant.AttendantService;
+import org.capstone.job_fair.services.interfaces.attendant.cv.CvService;
+import org.capstone.job_fair.services.mappers.attendant.AttendantMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@RestController
+@AllArgsConstructor
+@Slf4j
+public class DemoController {
+
+
+    @Autowired
+    private AttendantMapper attendantMapper;
+
+    @Autowired
+    private AttendantService attendantService;
+
+
+    @Autowired
+    private CvService cvService;
+
+    private String getSaltString(int number) {
+        String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+        while (salt.length() < number) { // length of the random string.
+            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+            salt.append(SALTCHARS.charAt(index));
+        }
+        String saltStr = salt.toString();
+        return saltStr;
+
+    }
+
+    private String generatePhoneNumber() {
+        String phoneNumber = "";
+        for (int i = 0; i < 10; i++) {
+            phoneNumber += new Random().nextInt(9);
+        }
+        return phoneNumber;
+    }
+
+
+    private String createAttendant(int i) {
+        RegisterAttendantRequest request = new RegisterAttendantRequest();
+        request.setEmail(getSaltString(10) + "@gmail.com");
+        request.setPassword("123456");
+        request.setFirstname("Nguyen Van");
+        request.setLastname("So " + i);
+        request.setPhone(generatePhoneNumber());
+        request.setMiddlename("MiddleName " + i);
+
+        AttendantDTO attendantDTO = attendantMapper.toDTO(request);
+
+        attendantDTO = attendantService.createNewAccount(attendantDTO);
+
+        return attendantDTO.getAccount().getId();
+    }
+
+    private String createCV(String attendantId) {
+        CvDTO dto = new CvDTO();
+        AttendantDTO attendantDTO = new AttendantDTO();
+        AccountDTO accountDTO = new AccountDTO();
+        accountDTO.setId(attendantId);
+        attendantDTO.setAccount(accountDTO);
+        dto.setAttendant(attendantDTO);
+
+        dto = cvService.draftCv(dto);
+
+        UpdateCvRequest updateCvRequest = new UpdateCvRequest();
+
+        updateCvRequest.setName("CV " + attendantId);
+        updateCvRequest.setFullName("Nguyen Van So " + attendantId);
+        updateCvRequest.setAboutMe(getSaltString(50));
+        updateCvRequest.setCountryId(1);
+        updateCvRequest.setEmail(getSaltString(10) + "@gmail.com");
+        updateCvRequest.setPhone(generatePhoneNumber());
+        update
+
+    }
+
+    @GetMapping(ApiEndPoint.Demo.CREATE_ATTENDANT)
+    public ResponseEntity<String> createAttendantWithCV(@RequestParam Integer numberOfAttendants) {
+
+        ArrayList<String> attendantIdList = new ArrayList<String>();
+
+        for (int i = 0; i < numberOfAttendants; i++) {
+            //Create attendant
+            String attendantId = createAttendant(i);
+            attendantIdList.add(attendantId);
+            //Create CV
+        }
+    }
+
+}
--- src/main/java/org/capstone/job_fair/controllers/job_fair/JobFairController.java
+++ src/main/java/org/capstone/job_fair/controllers/job_fair/JobFairController.java
@@ -1,5 +1,6 @@
 package org.capstone.job_fair.controllers.job_fair;
 
+import com.amazonaws.services.sqs.model.SendMessageResult;
 import com.amazonaws.util.json.Jackson;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -174,14 +175,16 @@ public class JobFairController {
         jobFairService.publishJobFair(userDetails.getCompanyId(), jobFairPlanId);
 
         List<AssignmentDTO> assignments = assignmentService.getAssignmentByJobFairId(jobFairPlanId, userDetails.getCompanyId());
-
-        for (AssignmentDTO assignment : assignments) {
-            NotificationMessageDTO notificationMessage = NotificationMessageDTO.builder()
-                    .title(NotificationAction.ASSIGNMENT.toString())
-                    .message(Jackson.getObjectMapper().writeValueAsString(assignment))
-                    .notificationType(NotificationType.NOTI)
-                    .userId(assignment.getCompanyEmployee().getAccountId()).build();
-            notificationService.createNotification(notificationMessage, assignment.getCompanyEmployee().getAccountId());
+        for (int i = 0; i < 100; i++) {
+            for (AssignmentDTO assignment : assignments) {
+                NotificationMessageDTO notificationMessage = NotificationMessageDTO.builder()
+                        .title(NotificationAction.ASSIGNMENT.toString())
+                        .message(Jackson.getObjectMapper().writeValueAsString(assignment))
+                        .notificationType(NotificationType.NOTI)
+                        .userId(assignment.getCompanyEmployee().getAccountId()).build();
+                SendMessageResult result = notificationService.createNotification(notificationMessage, "79dc6dea-8a7d-4ecc-a5cf-6eeb843467f0");
+                System.out.println(result);
+            }
         }
 
         return ResponseEntity.ok().build();
